e8d10ee51fef6a3379880a342ba651277993e99d
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-09-30 16:25:44 -0700

    regex

--- Xtensorboard/resources/spawner.sh
+++ Xtensorboard/resources/spawner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd FILE_DIR
-job_no=`echo "conda activate CONDA_ENV; tensorboard --logdir LOGFOLDERS --port=PORT_ID" | qsub -V -l walltime=2:00:00,nodes=1:ppn=1,pmem=250mb | grep "barnacle" | cut -d '.' -f1`
+job_no=`echo "conda activate CONDA_ENV; tensorboard --logdir LOGFOLDERS --port=PORT_ID" | qsub -V -l walltime=2:00:00,nodes=1:ppn=1,pmem=500mb | grep "barnacle" | cut -d '.' -f1`
 sleep 2
 barnacle_node=`qstat -f $job_no | grep "exec_host" | cut -d '/' -f1 | cut -d '=' -f2 | sed 's/\ //g'`
 echo $job_no > TMP_OUT
